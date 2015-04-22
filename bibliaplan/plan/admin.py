d195760aeaf2fcce0b052be6e459b3e68cd99993
Mateus <mateuspo10@gmail.com>
2015-04-22 18:50:35 -0300

    Admin

--- bibliaplan/plan/admin.py
+++ bibliaplan/plan/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from plan.models import Livros, Testamentos, Versoes, Versiculos
 
-# Register your models here.
+class VersiculoAdmin(admin.ModelAdmin):
+    list_display = ('texto', 'livro', 'versao')
+    search_fields = ['texto']
+
+class LivroAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'abreviacao', 'testamento')
+    search_fields = ['nome', 'abreviacao']
+
+class TestamentoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'book_count')
+
+    def book_count(self, obj):
+        return obj.livros_set.count()
+
+admin.site.register(Testamentos, TestamentoAdmin)
+admin.site.register(Livros, LivroAdmin)
+admin.site.register(Versiculos, VersiculoAdmin)
+admin.site.register(Versoes)
--- bibliaplan/plan/models.py
+++ bibliaplan/plan/models.py
@@ -2,16 +2,20 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-
 class Livros(models.Model):
     testamento = models.ForeignKey('Testamentos')
     posicao = models.IntegerField()
     nome = models.CharField(max_length=30)
-    abreviacao = models.CharField(max_length=5)
+    abreviacao = models.CharField(max_length=5, verbose_name = 'abreviação')
 
     class Meta:
         managed = False
         db_table = 'livros'
+        verbose_name_plural = 'Livros'
+        ordering = ['id']
+
+    def __str__(self):
+        return self.nome
 
 class Testamentos(models.Model):
     nome = models.CharField(max_length=30)
@@ -19,24 +23,37 @@ class Testamentos(models.Model):
     class Meta:
         managed = False
         db_table = 'testamentos'
+        verbose_name_plural = 'Testamentos'
+        ordering = ['nome']
+
+    def __str__(self):
+        return self.nome
 
 
 class Versiculos(models.Model):
-    versao_id = models.IntegerField()
+    versao = models.ForeignKey('Versoes')
     livro = models.ForeignKey(Livros)
-    capitulo = models.IntegerField()
-    versiculo = models.IntegerField()
+    capitulo = models.IntegerField(verbose_name = 'capítulo')
+    versiculo = models.IntegerField(verbose_name = 'versículo')
     texto = models.TextField()
 
     class Meta:
         managed = False
         db_table = 'versiculos'
+        verbose_name_plural = 'Versículos'
+
+    def __str__(self):
+        return "%s:%s - %s" % (self.capitulo, self.versiculo, self.texto)
 
 
 class Versoes(models.Model):
     vrs_id = models.AutoField(primary_key=True)
-    vrs_nome = models.CharField(max_length=50)
+    vrs_nome = models.CharField(max_length=50, verbose_name = 'nome da versão')
 
     class Meta:
         managed = False
         db_table = 'versoes'
+        verbose_name_plural = 'Versões'
+
+    def __str__(self):
+        return self.vrs_nome
